66e5394e248637e3b63391b1206c12672d92fcfb
syedzahidsaleem <syedzahidsaleem2@gmail.com>
2026-09-21 23:21:00 +0530

    feat(config): default discord_rpc_enabled to false

--- src/config.rs
+++ src/config.rs
@@ -72,7 +72,7 @@ impl Default for Config {
             addons_enabled: false,
             default_player: None,
             download_dir: None,
-            discord_rpc_enabled: true,
+            discord_rpc_enabled: false,
             notifications: NotificationsConfig::default(),
         }
     }
